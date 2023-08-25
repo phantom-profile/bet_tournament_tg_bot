9a74a2dfd9f549bdec6c2145f2aa89aab13b0c90
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-08-25 20:59:23 +0300

    added keyboards and text choices

--- bot.py
+++ bot.py
@@ -1,18 +1,62 @@
 import telebot
+from telebot import types
 from telebot.types import Message
 
 from config import env_variables
 from lib.locale_service import LocaleService
+from lib.backend_client import BackendClient
 
 
 bot = telebot.TeleBot(env_variables.get('TG_BOT_TOKEN'), parse_mode=None)
 localizator = LocaleService()
 
 
-@bot.message_handler(commands=['start', 'help'])
+def start_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    rules = types.KeyboardButton(text=localizator.read('rules'))
+    register = types.KeyboardButton(text=localizator.read('register'))
+    status = types.KeyboardButton(text=localizator.read('status'))
+    keyboard.add(rules, register, status)
+    return keyboard
+
+
+def participant_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    make_bet = types.KeyboardButton(text=localizator.read('make_bet'))
+    rules = types.KeyboardButton(text=localizator.read('rules'))
+    status = types.KeyboardButton(text=localizator.read('status'))
+    keyboard.add(make_bet, rules, status)
+    return keyboard
+
+
+def check_status(message: Message):
+    print(message.chat.id)
+    print(message.from_user.id)
+    client = BackendClient()
+    if client.in_current_tournament(message.from_user.id):
+        return bot.send_message(
+            message.chat.id,
+            localizator.read('participant'),
+            reply_markup=participant_keyboard()
+        )
+
+    bot.send_message(message.chat.id, localizator.read('viewer'), reply_markup=start_keyboard())
+
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message: Message):
     print(message.chat.id)
-    bot.reply_to(message, localizator.read('start'))
+    bot.send_message(message.chat.id, localizator.read('start'), reply_markup=start_keyboard())
+
+
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+    if message.text == localizator.read('rules'):
+        bot.send_message(message.chat.id, localizator.read('rules_faq'))
+    elif message.text == localizator.read('register'):
+        bot.send_message(message.chat.id, localizator.read('register_instruction'))
+    elif message.text == localizator.read('status'):
+        check_status(message)
 
 
 if __name__ == '__main__':
