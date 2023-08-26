0664a36f1551c6344e22fd6b2d8b56926c543818
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-08-26 20:48:24 +0300

    added base user model

--- event_handlers.py
+++ event_handlers.py
@@ -11,11 +11,20 @@ from lib.base_client import Red
 bot = TeleBot(env_variables.get('TG_BOT_TOKEN'), parse_mode=None)
 
 
+class User:
+    def __init__(self, user_id: int):
+        self.id = user_id
+
+    @property
+    def is_temp_blocked(self) -> bool:
+        return Red.exists(f"lock-interface-{self.id}")
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message: Message):
-    if Red.exists(f"lock-interface-{message.from_user.id}"):
-        bot.send_message(message.from_user.id, locale.read('pay_proof_message'))
-        return
+    user = User(message.from_user.id)
+    if user.is_temp_blocked:
+        return bot.send_message(user.id, locale.read('pay_proof_message'))
 
     initial_info(message.chat.id, bot)
 
@@ -23,18 +32,18 @@ def send_welcome(message: Message):
 @bot.message_handler(content_types=['text'])
 def message_reply(message: Message):
     print(message.text)
-    if Red.exists(f"lock-interface-{message.from_user.id}"):
-        bot.send_message(message.chat.id, locale.read('pay_proof_message'))
-        return
+    user = User(message.from_user.id)
+    if user.is_temp_blocked:
+        return bot.send_message(user.id, locale.read('pay_proof_message'))
 
     if message.text == locale.read('rules'):
-        rules_info(message.chat.id, bot)
+        rules_info(user.id, bot)
     elif message.text == locale.read('register'):
-        init_registration(message.from_user.id, bot)
+        init_registration(user.id, bot)
     elif message.text == locale.read('status'):
-        check_status(message.from_user.id, bot)
+        check_status(user.id, bot)
     elif message.text == locale.read('request_pay_proof'):
-        block_interface(message.from_user.id, bot)
+        block_interface(user.id, bot)
 
 
 @bot.message_handler(content_types=['document'])
