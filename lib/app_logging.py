8040301f74b6eff990e42bd9135ec0912b7a940f
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-08-27 21:08:41 +0300

    added loggers

---
+++ lib/app_logging.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from functools import wraps
+
+import sentry_sdk
+from telebot.types import Message
+
+
+def logger_factory():
+    def debug_request(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except Exception as e:
+                if not isinstance(e, KeyboardInterrupt):
+                    sentry_sdk.capture_exception(e)
+                raise e
+
+        return inner
+    return debug_request
+
+
+def log(message: Message):
+    print("INFO:", datetime.now())
+    print(
+        f"Сообщение от {message.from_user.first_name} {message.from_user.last_name}"
+        f" (id = {message.from_user.id}) \n {message.text}"
+    )
+    if message.document:
+        print(
+            f"Загрузка файла {message.document.file_name}. "
+            f"Размер: {message.document.file_size}"
+        )
+
