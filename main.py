0583cc49f96ccd6162a59752cead94f5a48a9fe7
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-12-31 16:48:45 +0600

    refactor endpoint

--- main.py
+++ main.py
@@ -11,34 +11,40 @@ app = Flask(__name__)
 
 
 @dataclass
-class SendParams:
+class SendMessagesService:
     chat_ids: list[int]
     message: str
+    token: str
     errors: list[str] = field(default_factory=list)
+    response: dict = None
+    status: int = None
 
     def __post_init__(self):
         if not isinstance(self.chat_ids, list):
             self.errors.append("chat_ids param required as list of ids")
         if not isinstance(self.message, str):
             self.errors.append("message param required as formatted string")
+        if self.token != env_variables.get('API_ACCESS_TOKEN'):
+            self.errors.append("Invalid access token")
 
-    def as_dict(self):
-        return {"chat_ids": self.chat_ids, "message": self.message}
+    def call(self):
+        if self.errors:
+            self.response = {"errors": self.errors}
+            self.status = 400
+            return
+        for chat_id in self.chat_ids:
+            bot.send_message(chat_id, f"{self.message}")
+            sleep(0.3)
+        self.response = {"chat_ids": self.chat_ids, "message": self.message}
+        self.status = 200
 
 
 @app.route("/tgsend", methods=['POST'])
-def hello_world():
-    if request.args.get('token') != env_variables.get('API_ACCESS_TOKEN'):
-        return {"errors": ["Invalid access token"]}, 403
-    params = SendParams(
+def send_message():
+    service = SendMessagesService(
         chat_ids=request.json.get('chat_ids'),
-        message=request.json.get('message')
+        message=request.json.get('message'),
+        token=request.args.get('token')
     )
-    if params.errors:
-        return {"errors": params.errors}, 422
-
-    for chat_id in params.chat_ids:
-        bot.send_message(chat_id, f"{params.message}")
-        sleep(0.3)
-
-    return params.as_dict()
+    service.call()
+    return service.response, service.status
