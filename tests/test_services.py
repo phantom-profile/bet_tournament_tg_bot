a9b277d1b2a43ad726280b0ccbd75e78bf346617
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2024-02-28 22:51:03 +0600

    tests for status service

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,37 +1,45 @@
+from bot_app.ui_components import Keyboards
 from lib.current_service import CurrentTournamentsService
+from lib.status_service import CheckStatusService
 from lib.spam_protection import SpamProtector
 
-from tests.factories import ResponceFactory
-from tests.helpers import tournament_payload
+from tests.factories import ResponceFactory, UserFactory
 
 
-def test_status_receiving():
-    pass
+def test_status_for_member(success_client, ui_stub):
+    user = UserFactory(member=True)
+    CheckStatusService(user=user, ui=ui_stub).call()
+    ui_stub.send.assert_called_once_with(message='member status', keyboard=Keyboards.MEMBER)
 
 
-def test_get_current_tournament(backend_client):
-    members = [
-        {'status': 'registered', 'tg_id': '123-123'},
-        {'status': 'approved', 'tg_id': '223-223'},
-        {'status': 'declined', 'tg_id': '323-323'}
-    ]
-    payload = tournament_payload(members=members, name='t_name')
-    response = ResponceFactory.create(body=payload)
+def test_status_for_guest(success_client, ui_stub):
+    user = UserFactory(guest=True)
+    CheckStatusService(user=user, ui=ui_stub).call()
+    ui_stub.send.assert_called_once_with(message='viewer status', keyboard=Keyboards.START)
+
+
+def test_status_for_closed_tournament(backend_client, empty_tournament_as_json, ui_stub):
+    user = UserFactory(guest=True)
+    response = ResponceFactory.create(body=empty_tournament_as_json)
     backend_client.get_current_tournaments.return_value = response
+    CheckStatusService(user=user, ui=ui_stub).call()
+    ui_stub.send.assert_called_once_with(message='registration closed error', keyboard=Keyboards.START)
 
+
+def test_get_current_tournament(success_client, tournament_as_json):
     result = CurrentTournamentsService().call()
 
     assert result is not None
     assert result.name == 't_name'
+    assert result.starts_at.year == 2024
     assert result.is_full() is False
-    assert result.members_ids == {'123-123', '223-223'}
-    assert result.is_member('123-123') is True
-    assert result.is_member('323-323') is False
+    assert result.members_ids == {'1', '2'}
+    assert result.is_member(1) is True
+    assert result.is_member(3) is False
 
 
-def test_get_current_tournament_if_does_not_exist(backend_client):
-    payload = tournament_payload(no_data=True)
-    response = ResponceFactory.create(body=payload)
+def test_get_current_tournament_if_does_not_exist(backend_client, empty_tournament_as_json):
+    response = ResponceFactory.create(body=empty_tournament_as_json)
     backend_client.get_current_tournaments.return_value = response
     result = CurrentTournamentsService().call()
     assert result is None
